43de2d53771fa8204162ab22ce0a147cfe24c989
Abhay Singh <ichbinabhaysingh1111@gmail.com>
2026-07-02 16:13:36 +0530

    DIVISIBITY OF A NUMBER BY ANOTHER

---
+++ divisibilty.py
@@ -0,0 +1,8 @@
+#program to test number divisible by another number 
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number"))
+remainder=num1%num2
+if remainder==0:
+    print(num1,"is divisible by",num2)
+else:
+    print(num1, "is not divisible by",num2)     
